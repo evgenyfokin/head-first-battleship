298203d12c79d5f910d22c09de8fe52e13b5d71e
evgenyfokin <fokin_e@inbox.ru>
2022-05-26 11:04:07 +0700

    added view object

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 'use strict'
+
+let view = {
+    displayMessage: function(msg) {
+        let messageArea = document.getElementById('messageArea');
+        messageArea.innerHTML = msg;
+    },
+    displayHit: function(location) {
+        let cell = document.getElementById(location);
+        cell.setAttribute('class', 'hit');
+    },
+    displayMiss: function(location) {
+        let cell = document.getElementById(location);
+        cell.setAttribute('class', 'miss')
+    }
+};
+
+
+let model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipSunk: 0,
+    ships: [
+        {locations: ['10', '20', '30'], hits: ['', '', '']},
+        {locations: ['32', '33', '34'], hits: ['', '', '']},
+        {locations: ['63', '64', '65'], hits: ['', '', '']},
+    ],
+    fire: function (guess) {
+        for (let i = 0; i < this.numShips; i++) {
+            let ship = this.ships[i];
+            let index = ship.locations.indexOf(guess)
+            if (index >= 0) {
+                ship.hits[index] = 'hit';
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
